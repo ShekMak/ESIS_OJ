0df461b7c18977ccdeef4b656a2f598074174469
sm_t <majorsonshekinah@gmail.com>
2018-04-19 08:28:27 +0300

    This file is so important for the take all comments in database

---
+++ controllers/all_commentaire.php
@@ -0,0 +1,9 @@
+<?php
+    require_once('../models/dao/commentaire.dao.php');
+    require_once('../models/dao/connexiondb.class.php');
+
+    $commentaireDAO = new CommentaireDAO();
+    $donnees = $commentaireDAO->getAllCommentaires($id);
+    $nombreCommentaire = count($donnees);
+
+?>
