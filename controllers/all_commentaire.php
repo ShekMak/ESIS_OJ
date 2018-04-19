cc9bad0d1f1a608bbaa08a2e899fcdcdf83f1629
sm_t <majorsonshekinah@gmail.com>
2018-04-19 07:04:38 +0300

    This file was replaced by all_publication

--- controllers/all_commentaire.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require_once('../models/dao/publication.dao.php');
-    require_once('../models/dao/connexiondb.class.php');
-
-    $publicationDAO = new PublicationDAO();
-    $data = $publicationDAO->getAllPublication();
-?>
