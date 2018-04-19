ec4fd36d176ead860baaf745556bcbff31834d35
sm_t <majorsonshekinah@gmail.com>
2018-04-19 05:19:09 +0300

    I add connexion in my file

--- controllers/add_publication.php
+++ controllers/add_publication.php
@@ -1,5 +1,26 @@
 <?php
+    session_start();
     require_once('../models/structure/publication.class.php');
     require_once('../models/dao/publication.dao.php');
-    require_once('../models/dao/etudiant.dao.php');
+    require_once('../models/dao/connexiondb.class.php');
 
+    if (isset($_SESSION['user'])){
+        if (isset($_POST['contenu']) && !empty($_POST['contenu'])){
+            $contenu = $_POST['contenu'];
+
+            $publication = new Publication(0,$_SESSION['user']['id'],$contenu,"",0,0);
+            $publicationDAO = new PublicationDAO();
+            $res = $publicationDAO->nouvellePublication($publication);
+
+            if ($res){
+                header('Location:../views/new.php?insert');
+            }else{
+                header('Location:../views/new.php?update');
+            }
+
+        }else{
+            echo "Erreur dans les donnees";
+        }
+    }else{
+        header('Location:index.php');
+    }
