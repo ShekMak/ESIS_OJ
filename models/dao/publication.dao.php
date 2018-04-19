71f3e53dbfe5f1a1311e9830a3c863a2911389cf
sm_t <majorsonshekinah@gmail.com>
2018-04-19 05:11:59 +0300

    I write a  function who will give possibility to add new publication and i gonna check it now

--- models/dao/publication.dao.php
+++ models/dao/publication.dao.php
@@ -12,7 +12,20 @@
 		}
 		
 		public function nouvellePublication($publication) {
-			
+            $str = "INSERT INTO publication VALUES(null, :idEtudiant, :contenu, now(), :nblike, :nbdislike)";
+            $req = $this->db->prepare($str);
+            $res = $req->execute(array(
+                'idEtudiant' => $publication->getIdEtudiant(),
+                'contenu' => $publication->getContenu(),
+                'nblike' => $publication->getNblike(),
+                'nbdislike' => $publication->getNbdislike()
+            ));
+
+            if($res) {
+                return True;
+            } else {
+                return False;
+            }
 		}
 		
 		public function top10() {
