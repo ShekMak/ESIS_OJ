d9a002909a485589094b37f671669dd4c3eaaceb
sm_t <majorsonshekinah@gmail.com>
2018-05-04 02:11:26 +0300

    I move there code in there views

--- controllers/add_dislike.php
+++ controllers/add_dislike.php
@@ -1,10 +1,4 @@
 <?php
     $publicationDAO = new PublicationDAO();
     $res = $publicationDAO->dislike($publication);
-
-    if (res){
-        header('Location: all.php');
-    }else{
-        header('Location: all.php?failed');
-    }
 ?>
