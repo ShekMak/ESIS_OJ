ee40e1197150b03fedc1d9be25ed7ad6a98b4da7
sm_t <majorsonshekinah@gmail.com>
2018-05-04 02:14:08 +0300

    I add failed state

--- views/top10.php
+++ views/top10.php
@@ -11,6 +11,13 @@
 		<?php include_once('head.php'); ?>
 		
 		<div class="content">
+            <?php
+            if (isset($_GET['failed'])){
+                echo '<p><br/><em>Votre opération a échouée!Veuillez recommencer s\'il vous plaît</em></p>';
+            }else{
+
+            }
+            ?>
 			<div class="toutes-publications">
 				<h2>Top 10</h2>
                 <?php
@@ -22,8 +29,8 @@
                     <p class="post-like">
                         <em>Posté le <?=$d['date']?></em>
                         <span class="like-dislike">
-                            <a href="like.php">Like</a>(<?=$d['nblike']?>) |
-                            <a href="dislike.php">Dislike</a>(<?=$d['nbdislike']?>)
+                            <a href="like.php?_id=<?=$d['id']?>">Like</a>(<?=$d['nblike']?>) |
+                            <a href="dislike.php?_id=<?=$d['id']?>">Dislike</a>(<?=$d['nbdislike']?>)
                         </span>
                     </p>
                 <?php } ?>
