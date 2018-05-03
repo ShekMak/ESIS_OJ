2af504ac07a7d9c9d86dc5e584db51117b3f2ae1
sm_t <majorsonshekinah@gmail.com>
2018-05-04 01:52:41 +0300

    I modify name of my session

--- controllers/add_commentaire.php
+++ controllers/add_commentaire.php
@@ -1,1 +1,9 @@
 <?php
+    require_once('../models/structure/commentaire.class.php');
+    require_once('../models/dao/commentaire.dao.php');
+    require_once('../models/dao/etudiant.dao.php');
+
+    if (isset($_POST['contenu']) && !empty($_POST['contenu'])){
+        $contenu = $_POST['contenu'];
+
+    }
