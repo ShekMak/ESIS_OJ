1c73eaa8e1390865e22b6e4fe85f677becbf6358
sm_t <majorsonshekinah@gmail.com>
2018-05-04 02:12:31 +0300

    I put code of there controller here

--- views/dislike.php
+++ views/dislike.php
@@ -7,7 +7,21 @@ if (isset($_GET['id'])){
     $id = $_GET['id'];
     $publication = new Publication($id,0,'','',0,0);
     include_once('../controllers/add_dislike.php');
-}else{
+    if (res){
+        header('Location: all.php');
+    }else{
+        header('Location: all.php?failed');
+    }
+}elseif(isset($_GET['_id'])){
+    $id = $_GET['_id'];
+    $publication = new Publication($id,0,'','',0,0);
+    include_once('../controllers/add_dislike.php');
+    if (res){
+        header('Location: top10.php');
+    }else{
+        header('Location: top10.php?failed');
+    }
+} else{
     header('Location: all.php');
 }
 ?>
--- views/like.php
+++ views/like.php
@@ -7,6 +7,20 @@
         $id = $_GET['id'];
         $publication = new Publication($id,0,'','',0,0);
         include_once('../controllers/add_like.php');
+        if (res){
+            header('Location: all.php');
+        }else{
+            header('Location: all.php?failed');
+        }
+    }elseif (isset($_GET['_id'])){
+        $id = $_GET['_id'];
+        $publication = new Publication($id,0,'','',0,0);
+        include_once('../controllers/add_like.php');
+        if (res){
+            header('Location: top10.php');
+        }else{
+            header('Location: top10.php?failed');
+        }
     }else{
         header('Location: all.php');
     }
