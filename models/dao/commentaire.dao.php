1c2349b4f703617a9fddfb2be270d8e176ece67c
sm_t <majorsonshekinah@gmail.com>
2018-05-05 07:44:23 +0300

    I finish to code this class

--- models/dao/commentaire.dao.php
+++ models/dao/commentaire.dao.php
@@ -8,7 +8,21 @@
 		}
 		
 		public function ajouterCommentaire($commentaire) {
-			
+            $str = "INSERT INTO commentaire VALUES(null, :idEtudiant, :idPublication, :contenu, now(), :nblike, :nbdislike)";
+            $req = $this->db->prepare($str);
+            $res = $req->execute(array(
+                'idEtudiant' => $commentaire->getIdEtudiant(),
+                'idPublication' => $commentaire->getIdPublication(),
+                'contenu' => $commentaire->getContenu(),
+                'nblike' => $commentaire->getNblike(),
+                'nbdislike' => $commentaire->getNbdislike()
+            ));
+
+            if($res) {
+                return True;
+            } else {
+                return False;
+            }
 		}
 		
 		public function getAllCommentaires($idPublication) {
@@ -21,11 +35,31 @@
 		}
 		
 		public function like($commentaire) {
-			
+            $str = "UPDATE commentaire SET nblike = nblike + 1 WHERE id = :idCommentaire";
+            $req = $this->db->prepare($str);
+            $res = $req->execute(array(
+                'idCommentaire' => $commentaire->getId()
+            ));
+
+            if($res) {
+                return True;
+            } else {
+                return False;
+            }
 		}
 		
 		public function dislike($commentaire) {
-			
+            $str = "UPDATE commentaire SET nbdislike = nbdislike + 1 WHERE id = :idCommentaire";
+            $req = $this->db->prepare($str);
+            $res = $req->execute(array(
+                'idCommentaire' => $commentaire->getId()
+            ));
+
+            if($res) {
+                return True;
+            } else {
+                return False;
+            }
 		}
 	}
 ?>
